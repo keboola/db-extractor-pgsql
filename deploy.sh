84ecb52dc04a1e794d4c1ba1e91b1b78863485f0
pivnicek <marc@keboola.com>
2017-06-12 14:44:56 +0200

    ci:fix: missing devportal URL

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 docker pull quay.io/keboola/developer-portal-cli-v2:latest
-export REPOSITORY=`docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD  quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository keboola keboola.ex-db-pgsql`
+export REPOSITORY=`docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL=$KBC_DEVELOPERPORTAL_URL quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository keboola keboola.ex-db-pgsql`
 docker tag keboola/db-extractor-pgsql:latest $REPOSITORY:$TRAVIS_TAG
 docker tag keboola/db-extractor-pgsql:latest $REPOSITORY:latest
-eval $(docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login keboola keboola.ex-db-pgsql)
+eval $(docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL=$KBC_DEVELOPERPORTAL_URL quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login keboola keboola.ex-db-pgsql)
 docker push $REPOSITORY:$TRAVIS_TAG
 docker push $REPOSITORY:latest
