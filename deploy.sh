df75f1ac78504b7caac9fae927f00e3af8944804
pivnicek <marc@keboola.com>
2017-08-03 17:07:59 +0200

    CI: add job tests to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 docker pull quay.io/keboola/developer-portal-cli-v2:latest
 export REPOSITORY=`docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL=$KBC_DEVELOPERPORTAL_URL quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository keboola keboola.ex-db-pgsql`
 docker tag keboola/db-extractor-pgsql:latest $REPOSITORY:$TRAVIS_TAG
@@ -5,3 +6,40 @@ docker tag keboola/db-extractor-pgsql:latest $REPOSITORY:latest
 eval $(docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL=$KBC_DEVELOPERPORTAL_URL quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login keboola keboola.ex-db-pgsql)
 docker push $REPOSITORY:$TRAVIS_TAG
 docker push $REPOSITORY:latest
+
+export SYRUP_CLI=quay.io/keboola/syrup-cli
+
+docker pull $SYRUP_CLI:latest
+
+# helper to keep travis from timing out
+function bell {
+  while true; do
+    echo "."
+    sleep 60
+  done
+}
+
+echo 'Running simple configuration job[1/1] ...'
+bell & docker run --rm -e KBC_STORAGE_TOKEN=$KBC_SYRUP_CLI_TOKEN \
+   $SYRUP_CLI:latest run-job keboola.ex-db-pgsql 285843250 $TRAVIS_TAG
+
+if [ $? -ne 0 ]; then
+  echo 'Simple test job run failed'
+  exit 1;
+fi
+
+echo 'Running ssh configuration job[2/2] ...'
+bell & docker run --rm -e KBC_STORAGE_TOKEN=$KBC_SYRUP_CLI_TOKEN \
+   $SYRUP_CLI:latest run-job keboola.ex-db-pgsql 287599200 $TRAVIS_TAG
+
+if [ $? -ne 0 ]; then
+  echo 'SSH test job run failed'
+  exit 1;
+fi
+
+echo 'All test jobs were successfull.  Updating repository tag in the developer portal...'
+docker run --rm \
+  -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME \
+  -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD \
+  -e KBC_DEVELOPERPORTAL_URL=$KBC_DEVELOPERPORTAL_URL \
+  quay.io/keboola/developer-portal-cli-v2:latest update-app-repository keboola keboola.ex-db-pgsql $TRAVIS_TAG
