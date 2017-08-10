3adc18100e907606b57fb3e75550ba153ff4c691
pivnicek <marc@keboola.com>
2017-08-10 15:51:08 +0200

    fix: add error reporting to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+
+set -e
+
 docker pull quay.io/keboola/developer-portal-cli-v2:latest
 export REPOSITORY=`docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL=$KBC_DEVELOPERPORTAL_URL quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository keboola keboola.ex-db-pgsql`
-docker tag keboola/db-extractor-pgsql:latest $REPOSITORY:$TRAVIS_TAG
-docker tag keboola/db-extractor-pgsql:latest $REPOSITORY:latest
+docker tag keboola/ex-db-pgsql:latest $REPOSITORY:$TRAVIS_TAG
+docker tag keboola/ex-db-pgsql:latest $REPOSITORY:latest
 eval $(docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL=$KBC_DEVELOPERPORTAL_URL quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login keboola keboola.ex-db-pgsql)
 docker push $REPOSITORY:$TRAVIS_TAG
 docker push $REPOSITORY:latest
