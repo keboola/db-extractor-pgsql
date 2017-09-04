1c2660f0b00eff9b77ba35783c91391802c8d410
pivnicek <marc@keboola.com>
2017-09-04 15:38:05 +0200

    feat: add schema to table property

--- src/Keboola/DbExtractor/Extractor/PgSQL.php
+++ src/Keboola/DbExtractor/Extractor/PgSQL.php
@@ -224,9 +224,12 @@ class PgSQL extends Extractor
 
         if (!is_null($tables) && count($tables) > 0) {
             $sql .= sprintf(
-                " AND TABLE_NAME IN (%s)",
+                " AND TABLE_NAME IN (%s) AND TABLE_SCHEMA IN (%s)",
                 implode(',', array_map(function ($table) {
-                    return $this->db->quote($table);
+                    return $this->db->quote($table['tableName']);
+                }, $tables)),
+                implode(',', array_map(function ($table) {
+                    return $this->db->quote($table['schema']);
                 }, $tables))
             );
         }
@@ -296,17 +299,22 @@ class PgSQL extends Extractor
         return $tabledef;
     }
 
-    public function simpleQuery($table, $columns = array())
+    public function simpleQuery(array $table, array $columns = array())
     {
         if (count($columns) > 0) {
-            return sprintf("SELECT %s FROM %s",
+            return sprintf("SELECT %s FROM %s.%s",
                 implode(', ', array_map(function ($column) {
                     return $this->quote($column);
                 }, $columns)),
-                $this->quote($table)
+                $this->quote($table['schema']),
+                $this->quote($table['tableName'])
             );
         } else {
-            return sprintf("SELECT * FROM %s", $this->quote($table));
+            return sprintf(
+                "SELECT * FROM %s.%s",
+                $this->quote($table['schema']),
+                $this->quote($table['tableName'])
+            );
         }
     }
     private function quote($obj) {
