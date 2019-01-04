134e7771ae36ce157e36ab8c36a200820b9dedb8
pivnicek <marc@keboola.com>
2019-01-04 15:46:11 +0100

    refactor tests

--- tests/Keboola/Extractor/ApplicationTest.php
+++ tests/Keboola/Extractor/ApplicationTest.php
@@ -4,115 +4,91 @@ declare(strict_types=1);
 
 namespace Keboola\DbExtractor\Tests;
 
-use Keboola\DbExtractor\Test\ExtractorTest;
 use Keboola\Csv\CsvFile;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Yaml\Yaml;
+use Keboola\DbExtractor\Exception\UserException;
 
-class ApplicationTest extends ExtractorTest
+class ApplicationTest extends BaseTest
 {
-    /** @var array */
-    private $dbConfig;
-
-    /** @var  string */
-    protected $rootPath;
-
-    /** @var string  */
-    protected $dataDir = __DIR__ . '/../../data';
-
-    public function setUp(): void
+    private function replaceConfig(array $config, string $format): void
     {
-        $this->rootPath = __DIR__ . '/../../..';
-
-        parent::setUp();
-        if (getenv('EXTERNAL_PG_HOST') === false) {
-            $this->fail("Missing environment var 'EXTERNAL_PG_HOST'");
+        @unlink($this->dataDir . '/config.json');
+        @unlink($this->dataDir . '/config.yml');
+        if ($format === self::CONFIG_FORMAT_JSON) {
+            file_put_contents($this->dataDir . '/config.json', json_encode($config));
+        } else if ($format === self::CONFIG_FORMAT_YAML) {
+            file_put_contents($this->dataDir . '/config.yml', Yaml::dump($config));
+        } else {
+            throw new UserException("Invalid config format type [{$format}]");
         }
-        if (getenv('EXTERNAL_PG_DATABASE') === false) {
-            $this->fail("Missing environment var 'EXTERNAL_PG_DATABASE'");
-        }
-        if (getenv('EXTERNAL_PG_USER') === false) {
-            $this->fail("Missing environment var 'EXTERNAL_PG_USER'");
-        }
-        if (getenv('EXTERNAL_PG_PASSWORD') === false) {
-            $this->fail("Missing environment var 'EXTERNAL_PG_PASSWORD'");
-        }
-        $this->dbConfig['host'] = getenv('EXTERNAL_PG_HOST');
-        $this->dbConfig['database'] = getenv('EXTERNAL_PG_DATABASE');
-        $this->dbConfig['user'] = getenv('EXTERNAL_PG_USER');
-        $this->dbConfig['password'] = getenv('EXTERNAL_PG_PASSWORD');
-        $this->dbConfig['port'] = (!is_null(getenv('EXTERNAL_PG_PORT'))) ? getenv('EXTERNAL_PG_PORT') : 5432;
     }
 
-    public function testRunAction(): void
+    /**
+     * @dataProvider configProvider
+     */
+    public function testRunAction(array $config, string $format): void
     {
-        $outputCsvFile = new CsvFile($this->dataDir . '/out/tables/in.c-main.info_schema.csv');
-        $manifestFile = $this->dataDir . '/out/tables/in.c-main.info_schema.csv.manifest';
-        @unlink($outputCsvFile->getPathname());
-        @unlink($manifestFile);
-
-        $config = Yaml::parse(file_get_contents($this->dataDir . '/pgsql/external_config.yml'));
-        $config['parameters']['db'] = $this->dbConfig;
-        @unlink($this->dataDir . '/config.yml');
-        file_put_contents($this->dataDir . '/config.yml', Yaml::dump($config));
-
+        $this->replaceConfig($config, $format);
         $process = Process::fromShellCommandline('php ' . $this->rootPath . '/run.php --data=' . $this->dataDir);
         $process->setTimeout(300);
-        $process->run();
+        $process->mustRun();
 
+        var_export($process->getErrorOutput());
+        var_export($process->getOutput());
         $this->assertEquals(0, $process->getExitCode());
         $this->assertEquals("", $process->getErrorOutput());
-
-        $this->assertTrue($outputCsvFile->isFile());
-        $this->assertFileExists($manifestFile);
     }
 
-    public function testTestConnectionAction(): void
+    /**
+     * @dataProvider configProvider
+     */
+    public function testTestConnectionAction(array $config, string $format): void
     {
-        $config = Yaml::parse(file_get_contents($this->dataDir . '/pgsql/external_config.yml'));
-        @unlink($this->dataDir . '/config.yml');
         $config['action'] = 'testConnection';
-        $config['parameters']['db'] = $this->dbConfig;
-        file_put_contents($this->dataDir . '/config.yml', Yaml::dump($config));
+        $this->replaceConfig($config, $format);
 
         $process = Process::fromShellCommandline('php ' . $this->rootPath . '/run.php --data=' . $this->dataDir);
         $process->setTimeout(300);
-        $process->run();
+        $process->mustRun();
         $this->assertJson($process->getOutput());
         $this->assertEquals(0, $process->getExitCode());
         $this->assertEquals("", $process->getErrorOutput());
     }
 
-    public function testTrailingSemicolonQuery(): void
+    /**
+     * @dataProvider configProvider
+     */
+    public function testTrailingSemicolonQuery(array $config, string $format): void
     {
-        $config = Yaml::parse(file_get_contents($this->dataDir . '/pgsql/external_config.yml'));
-        $config['parameters']['db'] = $this->dbConfig;
         $config['parameters']['tables'][0]['query'] = $config['parameters']['tables'][0]['query'] . ";";
-        @unlink($this->dataDir . '/config.yml');
-        file_put_contents($this->dataDir . '/config.yml', Yaml::dump($config));
-
+        $this->replaceConfig($config, $format);
         $process = Process::fromShellCommandline('php ' . $this->rootPath . '/run.php --data=' . $this->dataDir);
         $process->setTimeout(300);
-        $process->run();
+        $process->mustRun();
         $this->assertEquals(0, $process->getExitCode());
         $this->assertEquals("", $process->getErrorOutput());
     }
 
-    public function testProcessTimeout(): void
+    /**
+     * @dataProvider configProvider
+     */
+    public function testProcessTimeout(array $config, string $format): void
     {
-        $config = Yaml::parse(file_get_contents($this->dataDir . '/pgsql/external_config.yml'));
-        $config['parameters']['db'] = $this->dbConfig;
         $config['parameters']['tables'][0]['query'] = "SELECT pg_sleep(65), 1";
-        @unlink($this->dataDir . '/config.yml');
+        $this->replaceConfig($config, $format);
         file_put_contents($this->dataDir . '/config.yml', Yaml::dump($config));
 
         $process = Process::fromShellCommandline('php ' . $this->rootPath . '/run.php --data=' . $this->dataDir);
         $process->setTimeout(300);
-        $process->run();
+        $process->mustRun();
         $this->assertEquals(0, $process->getExitCode());
         $this->assertEquals("", $process->getErrorOutput());
     }
 
+    /**
+     * @dataProvider configProvider
+     */
     public function testUserError(): void
     {
         $config = Yaml::parse(file_get_contents($this->dataDir . '/pgsql/external_config.yml'));
@@ -137,8 +113,8 @@ class ApplicationTest extends ExtractorTest
         @unlink($outputCsvFile->getPathname());
         @unlink($manifestFile);
 
-        $config = Yaml::parse(file_get_contents($this->dataDir . '/pgsql/external_config.yml'));
-        $config['parameters']['db'] = $this->dbConfig;
+        $config = $this->getConfig();
+
         // queries with comments will break the () in the \copy command.
         // Failed \copy commands should fallback to using the old PDO method
         $config['parameters']['tables'][0]['query'] = "
@@ -147,8 +123,7 @@ class ApplicationTest extends ExtractorTest
                 select * from information_schema.columns
             ) as columns ON tables.table_schema = columns.table_schema AND tables.table_name = columns.table_name;
         ";
-        @unlink($this->dataDir . '/config.yml');
-        file_put_contents($this->dataDir . '/config.yml', Yaml::dump($config));
+        $this->replaceConfig($config, self::CONFIG_FORMAT_JSON);
 
         $process = Process::fromShellCommandline('php ' . $this->rootPath . '/run.php --data=' . $this->dataDir);
         $process->setTimeout(300);
@@ -162,17 +137,17 @@ class ApplicationTest extends ExtractorTest
         $this->assertFileExists($manifestFile);
     }
 
-    public function testGetTablesAction(): void
+    /**
+     * @dataProvider configProvider
+     */
+    public function testGetTablesAction(array $config, string $format): void
     {
-        $config = Yaml::parse(file_get_contents($this->dataDir . '/pgsql/external_config.yml'));
-        @unlink($this->dataDir . '/config.yml');
         $config['action'] = 'getTables';
-        $config['parameters']['db'] = $this->dbConfig;
-        file_put_contents($this->dataDir . '/config.yml', Yaml::dump($config));
+        $this->replaceConfig($config, $format);
 
         $process = Process::fromShellCommandline('php ' . $this->rootPath . '/run.php --data=' . $this->dataDir);
         $process->setTimeout(300);
-        $process->run();
+        $process->mustRun();
         $this->assertJson($process->getOutput());
 
         $this->assertEquals(0, $process->getExitCode());
---
+++ tests/Keboola/Extractor/BaseTest.php
@@ -0,0 +1,164 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\DbExtractor\Tests;
+
+use Keboola\DbExtractor\Test\ExtractorTest;
+
+use Keboola\DbExtractor\Application;
+use Keboola\DbExtractor\Logger;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Yaml\Yaml;
+
+class BaseTest extends ExtractorTest
+{
+    public const DRIVER = 'pgsql';
+
+    /** @var Application */
+    protected $app;
+
+    /** @var  string */
+    protected $rootPath;
+
+    /** @var string  */
+    protected $dataDir = __DIR__ . '/../../data';
+
+    /** @var  array */
+    protected $dbConfig;
+
+    protected function createDbProcess(string $query): Process
+    {
+        return new Process(
+            sprintf(
+                "PGPASSWORD='%s' psql -h %s -p %s -U %s -d %s -w -c \"$query\"",
+                $this->dbConfig['#password'],
+                $this->dbConfig['host'],
+                $this->dbConfig['port'],
+                $this->dbConfig['user'],
+                $this->dbConfig['database']
+            )
+        );
+    }
+
+    protected function runProcesses(array $processes): void
+    {
+        foreach ($processes as $process) {
+            $process->run();
+            if (!$process->isSuccessful()) {
+                $this->fail($process->getErrorOutput());
+            }
+        }
+    }
+
+    public function setUp(): void
+    {
+        $this->rootPath = '/code/';
+        $config = $this->getConfig();
+        $logger = new Logger('ex-db-pgsql-tests');
+        $this->app = new Application($config, $logger);
+
+        $this->dbConfig = $config['parameters']['db'];
+
+        // drop test tables
+        $processes = [];
+        // create a duplicate table in a different schema
+        $processes[] = $this->createDbProcess(
+            "CREATE SCHEMA IF NOT EXISTS testing;"
+        );
+        $processes[] = $this->createDbProcess(
+            "DROP TABLE IF EXISTS escaping;"
+        );
+        $processes[] = $this->createDbProcess(
+            "DROP TABLE IF EXISTS testing.escaping;"
+        );
+        $processes[] = $this->createDbProcess(
+            "DROP TABLE IF EXISTS types_fk;"
+        );
+        $processes[] = $this->createDbProcess(
+            "DROP TABLE IF EXISTS types;"
+        );
+
+        // create test tables
+        $processes[] = $this->createDbProcess(
+            "CREATE TABLE escaping (" .
+            "\"_funnycol\" varchar(123) NOT NULL DEFAULT 'column 1', " .
+            "\"_sadcol\" varchar(221) NOT NULL DEFAULT 'column 2', " .
+            "PRIMARY KEY (\"_funnycol\", \"_sadcol\"));"
+        );
+
+        $processes[] = $this->createDbProcess(
+            "\COPY escaping FROM 'vendor/keboola/db-extractor-common/tests/data/escaping.csv' WITH DELIMITER ',' CSV;"
+        );
+
+        $processes[] = $this->createDbProcess(
+            "CREATE TABLE types " .
+            "(character varchar(123) PRIMARY KEY, " .
+            "integer integer NOT NULL DEFAULT 42, " .
+            "decimal decimal(5,3) NOT NULL DEFAULT 1.2, " .
+            "date date DEFAULT NULL);"
+        );
+
+        $processes[] = $this->createDbProcess(
+            "\COPY types FROM 'tests/data/pgsql/types.csv' WITH DELIMITER ',' CSV HEADER;"
+        );
+
+        $processes[] = $this->createDbProcess(
+            "CREATE TABLE types_fk " .
+            "(character varchar(123) REFERENCES types (character), " .
+            "integer integer NOT NULL DEFAULT 42, " .
+            "decimal decimal(5,3) NOT NULL DEFAULT 1.2, " .
+            "date date DEFAULT NULL);"
+        );
+
+        $processes[] = $this->createDbProcess(
+            "\COPY types_fk FROM 'tests/data/pgsql/types.csv' WITH DELIMITER ',' CSV HEADER;"
+        );
+
+        $processes[] = $this->createDbProcess(
+            "CREATE TABLE testing.escaping (" .
+            "\"_funnycol\" varchar(123) NOT NULL DEFAULT 'column 1', " .
+            "\"_sadcol\" varchar(221) NOT NULL DEFAULT 'column 2', " .
+            "PRIMARY KEY (\"_funnY$-col\", \"_sadcol\"));"
+        );
+        $processes[] = $this->createDbProcess(
+            "\COPY testing.escaping FROM 'vendor/keboola/db-extractor-common/tests/data/escaping.csv' "
+            . "WITH DELIMITER ',' CSV HEADER;"
+        );
+
+        $this->runProcesses($processes);
+    }
+
+    public function getConfigRow(string $driver): array
+    {
+        $config = parent::getConfigRow($driver);
+        $config['parameters']['extractor_class'] = 'PgSQL';
+        return $config;
+    }
+
+    public function getConfig(string $driver = self::DRIVER, string $format = parent::CONFIG_FORMAT_YAML): array
+    {
+        $config = parent::getConfig($driver, $format);
+        $config['parameters']['extractor_class'] = 'PgSQL';
+        return $config;
+    }
+
+    public function configProvider(): array
+    {
+        $this->dataDir = __DIR__ . '/../../data';
+        return [
+            [
+                $this->getConfig(self::DRIVER, ExtractorTest::CONFIG_FORMAT_YAML),
+                ExtractorTest::CONFIG_FORMAT_YAML,
+            ],
+            [
+                $this->getConfig(self::DRIVER, ExtractorTest::CONFIG_FORMAT_JSON),
+                ExtractorTest::CONFIG_FORMAT_JSON,
+            ],
+            [
+                $this->getConfigRow(self::DRIVER),
+                ExtractorTest::CONFIG_FORMAT_JSON,
+            ],
+        ];
+    }
+}
