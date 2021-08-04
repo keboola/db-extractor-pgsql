3d5826ae2c0393a64e3952762bd8e13b9fa19dd8
Ondra Jodas <ondrej.jodas@keboola.com>
2021-08-04 11:52:21 +0200

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ GITHUB_TAG=${GITHUB_REF/refs\/tags\//}
 
 docker pull quay.io/keboola/developer-portal-cli-v2:latest
 export REPOSITORY=`docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository keboola keboola.ex-db-pgsql`
-docker tag keboola/ex-db-pgsql:latest $REPOSITORY:${GITHUB_TAG}
-docker tag keboola/ex-db-pgsql:latest $REPOSITORY:latest
+docker tag ${APP_IMAGE}:latest $REPOSITORY:${GITHUB_TAG}
+docker tag ${APP_IMAGE}:latest $REPOSITORY:latest
 eval $(docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login keboola keboola.ex-db-pgsql)
 docker push $REPOSITORY:${GITHUB_TAG}
 docker push $REPOSITORY:latest
