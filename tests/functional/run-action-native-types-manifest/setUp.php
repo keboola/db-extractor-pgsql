a17c2efc390f00c9b05d77d896f0e92156e67d90
Výborný Adam <mail@adamvyborny.cz>
2024-06-28 14:02:08 +0200

    Add test for new native types manifest format

---
+++ tests/functional/run-action-native-types-manifest/setUp.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Keboola\DbExtractor\FunctionalTests\DatabaseManager;
+use Keboola\DbExtractor\FunctionalTests\DatadirTest;
+
+return function (DatadirTest $test): void {
+    $manager = new DatabaseManager($test->getConnection());
+
+    // sales table
+    $manager->createSalesTable();
+    $manager->generateSalesRows();
+    $manager->addSalesConstraint('sales', ['createdat']);
+
+    // special table
+    $manager->createEscapingTable();
+    $manager->generateEscapingRows();
+};
